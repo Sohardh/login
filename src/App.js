2e0b28c06bedfdf0257ec657576cdcc75cfc6842
Sohardh <31403178+Sohardh@users.noreply.github.com>
2018-01-27 17:29:33 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+/* Sidebar.jsx */
+
+/* App.jsx */
+class App extends React.Component { 
   render() {
     return (
-      <form>
-      <input type = "submit" />
-      </form>
+      <div className='form'>
+        <div className='form_logo'>
+          Lo<span>g</span>o
+        </div>
+        <div className='form_title'>
+          Log<span>I</span>n
+        </div>
+        <form className='form_items'>
+          <div className='form_inputs'>
+            <input 
+              type='text'
+              required
+              />
+            <label>username or email</label>
+          </div>
+          <div className='form_inputs'>
+            <input
+              type='password'
+              required
+              />
+            <label>password</label>
+          </div>
+          <button className='form_button'>Log In</button>
+        </form>
+        <div className='form_other'>
+          <a href='#'>forgot password?</a>
+          <a href='#'>Join Now</a>
+        </div>
+      </div>
     );
   }
+  
 }
 
+/* main.js */
+class Main extends React.Component{
+  render(){
+    return (
+      <div>
+        <App />
+      </div>
+    );
+  }
+};
+
+
 export default App;
+
